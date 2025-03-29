560ef95a001e2945ab05fad721b624f6d2f5e7ef
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-29 08:09:58 +0530

    Update Lab_7.py

--- Lab_7.py
+++ Lab_7.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 # Load the summarization model
 @st.cache_resource
 def load_summarizer():
-    return pipeline("summarization", model="facebook/bart-large-cnn")
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
 summarizer = load_summarizer()
 
