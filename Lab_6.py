711d0013b8c59ef97e6fd6f606344bf33aef3c07
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-29 07:39:49 +0530

    Create Lab_6.py

---
+++ Lab_6.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from transformers import pipeline
+
+def main():
+    st.title("Hugging Face Model Demo")
+
+    # Create an input text box
+    input_text = st.text_input("Enter your text", "")
+
+    model = pipeline("sentiment-analysis", model = "distilbert-base-uncased-finetuned-sst-2-english")
+
+    # Create a button to trigger model inference
+    if st.button("Analyze"):
+        # Perform inference using the loaded model
+        result = model(input_text)
+        st.write("Prediction:", result[0]['label'], "| Score:", result[0]['score'])
+
+if __name__ == "__main__":
+    main()
